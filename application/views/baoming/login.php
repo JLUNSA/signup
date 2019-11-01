cf57d4f4bcc95bdb8ea3b718227e1af713c00b65
Nishikinor <33020146+Nishikinor@users.noreply.github.com>
2019-11-01 23:12:01 +0800

    Update login.php

--- application/views/baoming/login.php
+++ application/views/baoming/login.php
@@ -1,4 +1,45 @@
-登录页模板<br/>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <title>登录</title>
+  </head>
+  <body>
+    <div class="container">
+            <div class="col">
+              <form class="form-login" action="/login/" method="post">
+                  <h3 class="text-center">JLU-NSA查询页面</h3>
+                  <div class="form-group">
+                  	<label for="name">姓名：</label>
+                  	<input type="text" name="name" class="form-control" id="name" placeholder="Name" autofocus required>
+                  </div>
+                  <div class="form-group">
+                    <label for="id_username">教学号：</label>
+                    <input type="text" name='jxh' class="form-control" id="jxh" placeholder="Username" autofocus required>
+                  </div>
+                  <div class="form-group">
+                    <label for="id_password">密码：</label>
+                    <input type="password" name='password' class="form-control" id="password" placeholder="Password" required>
+                  </div>
+                <div>
+                  <button type="submit" class="btn btn-primary float-right">登录</button>
+                </div>
+              </form>
+            </div>
+    </div> <!-- /container -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
+    <script src="https://cdn.bootcss.com/popper.js/1.15.0/umd/popper.js"></script>
+    <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
+
 
 <?php
 echo form_error('jxh');
