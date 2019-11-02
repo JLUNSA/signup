ecdc0936ddf446f64038d3a08295b9e5170bbd32
kotori2 <admin@0x.mk>
2019-11-02 13:48:52 +0800

    修改session位置

--- application/config/config.php
+++ application/config/config.php
@@ -1,4 +1,5 @@
 <?php
+<?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
@@ -380,7 +381,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = APPPATH . 'session';
+$config['sess_save_path'] = APPPATH . '../session';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
