ac75b38f719def982a76ad7c637c319b566b1101
kotori2 <admin@0x.mk>
2019-11-05 16:08:37 +0800

    更新数据库

--- install.sql
+++ install.sql
@@ -1,14 +1,60 @@
-CREATE TABLE `baoming` (
-	`jxnum` VARCHAR(10) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`name` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`department` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`email` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`passwd` VARCHAR(65) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`ip` VARCHAR(60) NOT NULL COLLATE 'utf8mb4_general_ci',
-	`reserve_time` VARCHAR(100) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+CREATE TABLE `class` (
+	`class_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`detail` VARCHAR(500) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`date` DATE NOT NULL,
+	`start_select` TIMESTAMP NOT NULL,
+	`end_select` TIMESTAMP NOT NULL,
+	PRIMARY KEY (`class_id`)
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+CREATE TABLE `log` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`student_id` INT(11) NOT NULL,
+	`operation` VARCHAR(20) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`ip` VARCHAR(50) NOT NULL COMMENT 'v6/v4都可以' COLLATE 'utf8mb4_general_ci',
+	`result` VARCHAR(20) NOT NULL COMMENT 'OK/FAILED' COLLATE 'utf8mb4_general_ci',
+	`payload` VARCHAR(500) NOT NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=2
+;
+CREATE TABLE `selected_class` (
+	`student_num` INT(11) NOT NULL,
+	`subclass_id` INT(11) NOT NULL,
+	`select_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE INDEX `索引 1` (`student_num`, `subclass_id`)
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+CREATE TABLE `subclass` (
+	`subclass_id` INT(11) NOT NULL AUTO_INCREMENT,
+	`class_id` INT(11) NOT NULL,
+	`title` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`capacity` INT(11) NOT NULL DEFAULT '0',
+	`start_time` TIMESTAMP NOT NULL,
+	`end_time` TIMESTAMP NOT NULL,
+	PRIMARY KEY (`subclass_id`)
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+CREATE TABLE `user` (
+	`student_id` INT(11) NOT NULL,
+	`name` VARCHAR(30) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`department` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`email` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`password` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`photo` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_general_ci',
 	`verified` TINYINT(4) NOT NULL DEFAULT '0',
-	`time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (`jxnum`)
+	`is_admin` TINYINT(4) NOT NULL DEFAULT '0',
+	`insert_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`student_id`)
 )
 COLLATE='utf8mb4_general_ci'
 ENGINE=InnoDB
